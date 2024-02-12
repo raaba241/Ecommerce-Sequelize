67efbc83094e3c59b6e4cdf9b20640a5da43f0fe
unknown <raaba21@gmail.com>
2024-02-11 21:20:31 -0500

    added product route get all products

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,71 @@
 const router = require('express').Router();
+const { canTreatArrayAsAnd } = require('sequelize/types/lib/utils');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-});
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    })
+    res.json(categoryData)
+  } catch (err) {
+    res.json(err)
+  }
+})
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+
+    const singleCatagory = await Category.findByPk(
+      req.params.id, {
+      include: [{ model: Product }]
+    })
+  } catch (err) {
+    res.json(err)
+  }
+
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const postCatagory = await Category.create(req.body);
+    res.json(postCatagory)
+  } catch (err) {
+    res.json(err)
+  }
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    })
+  } catch (err) {
+    res.json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    await Category.destroy({
+      where: {
+        id: req.params.id,
+      }
+    })
+  }catch (err) {
+    res.json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,10 +5,18 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      { model: Category },
+      { model: Tag, through: ProductTag }
+    ],
+  })
+  .then(products => res.status(200).json(products))
+  .catch(err => {
+    console.error(err);
+    res.status(500).json(err);
+  });
 });
-
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
